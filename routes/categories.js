b4a03771c0bec835499dbd84a0695677a4a7b9b6
Dnaix <dv20345@gmail.com>
2022-06-09 17:09:38 -0500

    servicios y rutas de catgorias

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,49 @@
 const express = require('express')
+const res = require('express/lib/response')
 const router = express.Router()
 
-const CategoriesService = require('./../services/categories.services')
-const service = CategoriesService
+const categoriesService = require('./../services/categories.service')
+const service = new categoriesService()
 
-router.get('/:categoryId/products/:productId',(req,res)=>{
-  const categories = service.find()
+router.get('/',async (req,res)=>{
+
+  const categories = await service.find()
+  res.send(categories)
+})
+
+router.get('/filter',(req,res)=>{
+  res.send("Soy un filter")
+})
+
+router.get('/:id',async (req,res)=>{
+  const {id} = req.params
+  const categories = await service.findOne(id)
+
   res.json(categories)
 })
 
+router.post('/',async (req,res)=>{
+  const body = req.body
+  const newCategories = await service.create(body)
+  res.status(200).json(newCategories)
+})
+
+router.patch('/:id',async (req,res)=>{
+  try {
+    const {id} = req.params
+    const body = req.body
+    const categories = await service.update(id,body)
+    res.json(categories)
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    })
+  }
+})
+
+router.delete('/:id', async(req,res)=>{
+  const {id} = req.params
+  const rta = await service.delete(id)
+  res.json(rta)
+})
 module.exports = router
