582f3924c96f3e0665cd3e90b8cfb76b633c89a2
Fernandoc28 <fer.camacho.ur@gmail.com>
2022-06-09 16:08:34 -0500

    subiendo proyecto

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const productRouter = require('./products')
+const categoriesRouter = require('./categories')
+const userRouter = require('./users')
+
+
+
+function routerApi (app) {
+  const router = express.Router()
+  app.use('/api/v1',router)
+  router.use('/products',productRouter)
+  router.use('/categories',categoriesRouter)
+  router.use('/users',userRouter)
+}
+
+module.exports = routerApi
